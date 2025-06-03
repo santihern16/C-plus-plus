ef0478e2a5568b499fc6e5e0be5a0d95823ecf4f
Santiago Hernandez <sehernandezc@sanmateo.edu.co>
2025-06-03 16:52:36 -0500

    Add studentNotes.cpp for calculating average grades

---
+++ mini projects/studentNotes.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+double ingresarNotas(int materias)
+{
+    double sum{0};
+    for (size_t i = 0; i < materias; i++)
+    {
+        double nota;
+
+        std::cout << "Ingrese la nota: ";
+        std::cin >> nota;
+
+        sum += nota;
+    }
+
+    return sum;
+}
+
+int main()
+{
+    int materias{6};
+    double sum = ingresarNotas(materias);
+    double average = sum / materias;
+
+    std::cout << "Promedio de este semestre: " << average;
+}
